13bf9f6cd7975ca30c778d8009e178252a52c59f
Asad Khan <mrasad10khan@gmail.com>
2025-04-28 20:15:16 +0500

    Sidebar Styling

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -2,15 +2,21 @@ import { useState } from "react";
 import "./App.css";
 import Nav from "./Components/Nav/Nav";
 import Sidebar from "./Components/Sidebar/Sidebar";
+import { BrowserRouter, Routes, Route } from "react-router";
 import Layout from "./Layout/Layout";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="">
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout></Layout>}></Route>
+        <Route path="/settings" element={<Layout></Layout>}></Route>
+        <Route path="/my-boards" element={<Layout></Layout>}></Route>
+      </Routes>
       <Layout />
-    </div>
+    </BrowserRouter>
   );
 }
 
