1c9ee884a5b689373cc9785fe9aea56f71f7952a
Asad Khan <mrasad10khan@gmail.com>
2025-04-28 10:21:00 +0500

    Create Sidebar

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import "./App.css";
 import Nav from "./Components/Nav/Nav";
+import Sidebar from "./Components/Sidebar/Sidebar";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -8,6 +9,7 @@ function App() {
   return (
     <div className="overflow-hidden">
       <Nav />
+      <Sidebar />
     </div>
   );
 }
