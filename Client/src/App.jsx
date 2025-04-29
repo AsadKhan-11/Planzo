4987a19de23bb82cd9f63ab7667c43a38ce1163e
Asad Khan <mrasad10khan@gmail.com>
2025-04-29 10:02:11 +0500

    Create Dashboard

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -4,6 +4,7 @@ import Nav from "./Components/Nav/Nav";
 import Sidebar from "./Components/Sidebar/Sidebar";
 import { BrowserRouter, Routes, Route } from "react-router";
 import Layout from "./Layout/Layout";
+import Dashboard from "./Screens/Dashboard";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -11,11 +12,17 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Layout></Layout>}></Route>
-        <Route path="/settings" element={<Layout></Layout>}></Route>
-        <Route path="/my-boards" element={<Layout></Layout>}></Route>
+        <Route
+          path="/"
+          element={
+            <Layout>
+              <Dashboard />
+            </Layout>
+          }
+        ></Route>
+        {/* <Route path="/settings" element={<Layout></Layout>}></Route>
+        <Route path="/my-boards" element={<Layout></Layout>}></Route> */}
       </Routes>
-      <Layout />
     </BrowserRouter>
   );
 }
